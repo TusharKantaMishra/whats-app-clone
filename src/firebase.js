66b919d31d21971e65597af64788015958e8e0e5
Tushar <sipunm26@gmail.com>
2023-09-16 18:43:19 +0530

    whatsapp

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJp8rkZSJfjjLflGGyz_jW93fTFs91X70",
+    authDomain: "whatsapp-clone-36ad1.firebaseapp.com",
+    projectId: "whatsapp-clone-36ad1",
+    storageBucket: "whatsapp-clone-36ad1.appspot.com",
+    messagingSenderId: "1075843875753",
+    appId: "1:1075843875753:web:a7f9fa688c0a30dfe53ab8",
+    measurementId: "G-BE246R4661"
+  };
